3e1db820a413dc98d9aeeab38f5b5ee216af1b92
Ladibells <ladibell007@gmail.com>
2021-06-18 03:42:53 -0700

    quote update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>`
+#include <unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,6 +10,8 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
