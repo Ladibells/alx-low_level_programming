43f882be49f29a20c9fa43508f20c2a3c52a3f41
Ladibells <ladibell007@gmail.com>
2021-06-22 11:27:04 -0700

    updated task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	int i;
-	char p[] = "Holberton";
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(p[i]);
-	}
+	_putchar('H');
+	_putchar('0');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
