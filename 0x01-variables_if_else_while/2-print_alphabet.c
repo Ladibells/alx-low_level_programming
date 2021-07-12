d2465a6a8f59fc267d731b8caa07fdc4943c8e80
Ladibells <ladibell007@gmail.com>
2021-07-12 16:56:06 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+*main - entry code
+*return: 0 always (success)
+*/
+
+int main (void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+		putchar(10);
+	}
+
+	return (0);
+}
