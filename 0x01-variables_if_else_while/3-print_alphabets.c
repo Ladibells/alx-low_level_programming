3ac3d74aa7c00d76dbf0ed8210ea20ba536e6751
Ladibells <ladibell007@gmail.com>
2021-07-12 17:49:05 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry code
+*return: 0 always (success)
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+
+}
