a04c7385858a811bd9c51c4f6a8a0ba2c75508b4
Ladibells <ladibell007@gmail.com>
2021-07-14 11:49:08 -0700

    task 9 further modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,14 @@
 int main(void)
 {
 	int i;
-	int j;
 
-	for (i = 48; i <= 57; i++){
-
-		for (j = 48; j <= 57; j++)
-		{
-			putchar(i);
-			putchar(j);
-			putchar(44);
-			putchar(32);
-		}
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
 	}
-
+	
+	putchar(10);
 	return (0);
 }
