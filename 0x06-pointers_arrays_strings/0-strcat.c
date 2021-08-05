6cb5538ed551c18992e80a056d3f8e7379f8c5c9
Ladibells <ladibell007@gmail.com>
2021-08-05 04:37:26 -0700

    task 0 updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 	for(i = 0;dest[i] != '\0'; i++);
-	for(j = 0;dest[j] != '\0'; j++, i++)
+	for(j = 0;sr[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[i];
 	}
